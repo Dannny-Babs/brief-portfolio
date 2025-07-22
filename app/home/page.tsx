72b56456fdfbc303bf7eddda2703ccec76435b93
Daniel Babalola <dannybabs123456@gmail.com>
2025-07-22 18:15:54 -0400

    Enhance homepage content and layout: updated personal description, added detailed key achievements, and included selected works section with project highlights. Updated spell check words in VSCode settings for improved accuracy.

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -38,10 +38,10 @@ export default function Home() {
                             </p>
                         </div>
                         <div className="flex flex-col p-4">
-                            <p className="text-secondary-text text-lg leading-tight">
+                            <p className="text-primary-text text-lg leading-tight tracking-wide font-medium ">
                                 I&apos;m a design engineer based in Toronto, building products that are both functional and beautiful.
                                 Currently, I&apos;m working at Med Melanin as a design engineer, creating user-centered solutions for healthcare.
-                                I love building products that are both functional and beautiful.
+                                Over the past 3+ years, I&apos;ve built and shipped multiple high-impact projects across healthcare, accessibility, voice AI, and consumer apps—both as an in-house contributor and as a freelance partner to early-stage startups.
                             </p>
                         </div>
                     </div>
@@ -104,10 +104,12 @@ export default function Home() {
                         <span className="text-secondary-text">
                             Key Achievements:
                         </span>
-                        <ul className="list-disc list-inside text-primary-text text-lg tracking-loose pl-4 pr-4 leading-tight">
-                            <li>Designed and implemented a robust design system for the mobile and web apps</li>
-                            <li>Designed and built a website with 20+ pages, improving SEO rankings to the #1 spot on Google, significantly increasing platform visibility.</li>
-                            <li>Leveraged data from 200+ user stories to analyse pain points and deliver responsive, user-friendly features that improved engagement.</li>
+                        <ul className="list-disc list-inside pl-5 mt-1 space-y-1 text-lg leading-relaxed tracking-loose text-primary-text">
+                            <li>Spearheaded full-stack design &amp; dev of a 30+ page website (blog, CMS, SEO-optimized), ranking #1 for brand and health keywords.</li>
+                            <li>Integrated marketing tools like Mailchimp, Notion, and built custom automation scripts for internal workflows.</li>
+                            <li>Designed and maintained the complete web app dashboard, design system, and mobile app experience for patients.</li>
+                            <li>Collaborated directly with devs to ensure UI consistency, accessibility, and high implementation fidelity.</li>
+                            <li>Designed fundraising pitch decks &amp; visuals used in pitch competitions—contributed to raising over $50K+.</li>
                         </ul>
                     </p>
                 </div>
@@ -129,19 +131,169 @@ export default function Home() {
                         </p>
                     </div>
                     <p className="text-primary-text text-lg leading-tight">
-                        Worked as a designer and developer with early-stage founders and lean startup teams. Designed and shipped landing pages, brand sites, and interactive MVPs across industries. Always focused on shipping clean, functional products that people actually use.
+                        Worked as a designer and developer with early-stage founders and lean startup teams.
+                        I&apos;ve led the end-to-end design and development of production-grade websites, mobile apps, internal dashboards, and custom CMS platforms. My work spans web performance, SEO optimization, responsive front-end engineering, and co-creating internal design systems that scale.
                         <br />
                         <br />
                         <span className="text-secondary-text">
                             Key Achievements:
                         </span>
-                        <ul className="list-disc list-inside  text-primary-text text-lg tracking-loose pl-4 pr-4 leading-tight">
-                            <li>Designed and developed various websites and landing pages for clients</li>
-                            <li>Led the design and development of an Event Discovery mobile app, managing a cross-functional team of 5 to deliver a fully functional product within deadlines.</li>
-                            <li>Designed and built an inclusive E-commerce mobile app for neurodivergent users, leveraging Flutter and Firebase to deliver a seamless, accessible experience across iOS and Android. Focused on intuitive usability and high performance to empower a diverse audience.</li>
-                            <li>Designed various dashboards for clients, including a dashboard for an AI agent tool, a dashboard for an event management tool, and a dashboard for a social media management tool.</li>
+                        <ul className="list-disc list-inside  text-primary-text text-lg tracking-wide pl-4 pr-4 leading-relaxed">
+                            <li>Built and designed landing pages and full websites for 10+ early-stage teams.</li>
+                            <li>Designed dashboard for a voice AI agent platform, aligning UX with NLP user flows.</li>
+                            <li>Led design of a web app &amp; pitch deck for an AI-powered accessibility compliance checker.</li>
+                            <li>Created a creator analytics dashboard to help influencers and content teams optimize their social media growth.</li>
+                            <li>Designed &amp; built a Flutter mobile e-commerce app tailored for neurodivergent users (cross-platform, inclusive UX).</li>
+                            <li>Co-led a 5-person team to design an event discovery platform, owning product UI and front-end architecture.</li>
                         </ul>
                     </p>
+                </div>
+            </div>
+            <div className={cn(
+                "max-w-5xl border-r-[1.5px] border-l-[1.5px] border-b-[1.5px] border-dark-border mx-auto px-3 py-3",
+                "flex flex-row items-center w-full justify-between"
+            )}>
+                <div className="flex flex-col gap-2">
+                    <h3 className="text-lg font-medium text-secondary-text ">
+                        SELECTED WORKS
+                    </h3>
+                </div>
+            </div>
+            <div className="w-full max-w-5xl border-r-[1.5px] border-l-[1.5px] border-b-[1.5px] border-dark-border mx-auto grid grid-cols-1 md:grid-cols-3 ">
+                <div className="flex flex-col h-full col-span-1">
+                    <div className="relative w-full aspect-[4/3] border-r-[1.5px] border-b-[1.5px] border-dark-border overflow-hidden">
+                        <Image src="/images/work1.png" alt="work1" fill className="object-cover" />
+                        <div className="absolute bottom-0 left-0 right-0 bg-dark-background/80 p-4">
+                            <h4 className="text-primary-text tracking-wide text-lg font-medium leading-tight">
+                                MedMelanin Website
+                            </h4>
+                            <p className="text-secondary-text text-[15px] font-medium leading-tight">
+                                30+ page site with CMS, SEO, automations.                            </p>
+                        </div>
+                    </div>
+                    <div className="relative w-full aspect-[4/3] border-r-[1.5px] border-b-[1.5px] border-dark-border overflow-hidden">
+                        <Image src="/images/work1.png" alt="work1" fill className="object-cover" />
+                        <div className="absolute bottom-0 left-0 right-0 bg-dark-background/80 p-4">
+                            <h4 className="text-primary-text tracking-wide text-lg font-medium leading-tight">
+                                Inclusivio
+                            </h4>
+                            <p className="text-secondary-text text-[15px] font-medium leading-tight">
+                                Accessibility compliance checker with full dashboard UX
+                            </p>
+                        </div>
+                    </div>
+                    <div className="relative w-full aspect-[4/3] border-r-[1.5px]  border-dark-border overflow-hidden">
+                        <Image src="/images/work1.png" alt="work1" fill className="object-cover" />
+                        <div className="absolute bottom-0 left-0 right-0 bg-dark-background/80 p-4">
+                            <h4 className="text-primary-text tracking-wide text-lg font-medium leading-tight">
+                                Talo
+                            </h4>
+                            <p className="text-secondary-text text-[15px] font-medium leading-tight">
+                                Job search and ATS platform with AI insights
+                            </p>
+                        </div>
+                    </div>
+
+                </div>
+                <div className="flex flex-col h-full col-span-1">
+                    <div className="relative w-full aspect-[4/3] border-r-[1.5px] border-b-[1.5px] border-dark-border overflow-hidden">
+                        <Image src="/images/work1.png" alt="work1" fill className="object-cover" />
+                        <div className="absolute bottom-0 left-0 right-0 bg-dark-background/80 p-4">
+                            <h4 className="text-primary-text tracking-wide text-lg font-medium leading-tight">
+                                Sortee
+                            </h4>
+                            <p className="text-secondary-text text-[15px] font-medium leading-tight">
+                                Pitch Decks + Brand Design Exploration for a startup
+                            </p>
+                        </div>
+                    </div>
+                    <div className="relative w-full aspect-[4/3] border-r-[1.5px] border-b-[1.5px] border-dark-border overflow-hidden">
+                        <Image src="/images/work1.png" alt="work1" fill className="object-cover" />
+                        <div className="absolute bottom-0 left-0 right-0 bg-dark-background/80 p-4">
+                            <h4 className="text-primary-text tracking-wide text-lg font-medium leading-tight">
+                                MedMelanin App
+                            </h4>
+                            <p className="text-secondary-text text-[15px] font-medium leading-tight">
+                                Health advocacy platform designed for women of colour
+                            </p>
+                        </div>
+                    </div>
+
+                    <div className="relative w-full aspect-[4/3] border-r-[1.5px]  border-dark-border overflow-hidden">
+                        <Image src="/images/work1.png" alt="work1" fill className="object-cover" />
+                        <div className="absolute bottom-0 left-0 right-0 bg-dark-background/80 p-4">
+                            <h4 className="text-primary-text tracking-wide text-lg font-medium leading-tight">
+                                Spectraum
+                            </h4>
+                            <p className="text-secondary-text text-[15px] font-medium leading-tight">
+                                E-commerce app for neurodivergent users
+                            </p>
+                        </div>
+                    </div>
+
+                </div>
+                <div className="flex flex-col h-full col-span-1">
+                    <div className="relative w-full aspect-[4/3]  border-b-[1.5px] border-dark-border overflow-hidden">
+                        <Image src="/images/work3.png" alt="work3" fill className="object-cover" />
+                        <div className="absolute bottom-0 left-0 right-0 bg-dark-background/80 p-4">
+                            <h4 className="text-primary-text tracking-wide text-lg font-medium leading-tight">
+                                Marathon
+                            </h4>
+                            <p className="text-secondary-text text-[15px] font-medium leading-tight">
+                                AI Sales Enablement Platform
+                            </p>
+                        </div>
+                    </div>
+                    <div className="relative w-full aspect-[4/3]  border-b-[1.5px] border-dark-border overflow-hidden">
+                        <Image src="/images/work1.png" alt="work1" fill className="object-cover" />
+                        <div className="absolute bottom-0 left-0 right-0 bg-dark-background/80 p-4">
+                            <h4 className="text-primary-text tracking-wide text-lg font-medium leading-tight">
+                                Tally
+                            </h4>
+                            <p className="text-secondary-text text-[15px] font-medium leading-tight">
+                                Personal finance tracker with AI insights
+                            </p>
+                        </div>
+                    </div>
+                    <div className="relative w-full aspect-[4/3]   border-dark-border overflow-hidden">
+                        <Image src="/images/work1.png" alt="work1" fill className="object-cover" />
+                        <div className="absolute bottom-0 left-0 right-0 bg-dark-background/80 p-4">
+                            <h4 className="text-primary-text tracking-wide text-lg font-medium leading-tight">
+                                Juniper
+                            </h4>
+                            <p className="text-secondary-text text-[15px] font-medium leading-tight">
+                                Smart apartment search with onboarding & investment tools
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div className={cn(
+                "max-w-5xl border-r-[1.5px] border-l-[1.5px] border-b-[1.5px] border-dark-border mx-auto md:p-8 p-3 ",
+                "flex flex-row items-center w-full justify-between"
+            )}>
+                <div className="flex flex-col gap-2 text-left md:w-1/2">
+                    <h5 className="text-lg font-medium text-primary-text ">
+                        I&apos;m Daniel. A pixel alchemist, problem solver, and passionate human.
+                        I design, build, and learn in public. Imperfectly perfect and always evolving.
+                        
+                        <br />
+                        In my free time, I create posters and visuals just for fun.
+                        <br />
+                        <br />
+                        Beyond the screen, I&apos;m chasing bold ideas and building things that matter.
+                        <br />
+                        <br />
+                        Thanks for being here.
+
+                    </h5>
+                   
+                      
+                </div>
+                <div className="flex flex-col gap-2">
+                    <div className="flex flex-row items-center gap-2">
+                        <Image src="/images/linkedin.png" alt="linkedin" width={24} height={24} />
+                    </div>
                 </div>
             </div>
         </div>
